cf719d14b4684706bf4ce1fb4d4bc989ef814d3e
farrux2213 <asqaraliyevfarrux2213@gmail.com>
2024-04-02 18:18:07 +0500

    two sum problem

---
+++ index.js
@@ -0,0 +1,14 @@
+const nums = [15, 7, 11, 2];
+const target = 9;
+function twoSum(nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+  return [];
+}
+
+console.log(twoSum(nums, target));
